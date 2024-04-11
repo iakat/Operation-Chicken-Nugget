23f0b6f4dcbcc7b24b426b22bcc8402a18020266
Katia Esposito <katia@linux.com>
2024-04-11 21:25:48 +0200

    formatting

--- consumerKey.py
+++ consumerKey.py
@@ -1,13 +1,13 @@
 import json, ovh
 
-with open('config.json') as f:
+with open("config.json") as f:
     config = json.load(f)
 
 # create a client using configuration
 client = ovh.Client(
-    endpoint=config['endpoint'],
-    application_key=config['application_key'],
-    application_secret=config['application_secret']
+    endpoint=config["endpoint"],
+    application_key=config["application_key"],
+    application_secret=config["application_secret"],
 )
 
 # Request RO, /me API access
@@ -17,9 +17,9 @@ ck.add_recursive_rules(ovh.API_READ_WRITE, "/")
 # Request token
 validation = ck.request()
 
-print("Please visit %s to authenticate" % validation['validationUrl'])
+print("Please visit %s to authenticate" % validation["validationUrl"])
 input("and press Enter to continue...")
 
 # Print nice welcome message
-print("Welcome", client.get('/me')['firstname'])
-print("Btw, your 'consumerKey' is '%s'" % validation['consumerKey'])
+print("Welcome", client.get("/me")["firstname"])
+print("Btw, your 'consumerKey' is '%s'" % validation["consumerKey"])
--- nugget.py
+++ nugget.py
@@ -1,27 +1,39 @@
-import requests, hashlib, json, time, ovh
+import hashlib
+import json
+import time
 from datetime import datetime
 from random import randint
 
-with open('config.json') as f:
+import ovh
+import requests
+
+with open("config.json") as f:
     config = json.load(f)
 
 # Instantiate. Visit https://api.ovh.com/createToken/?GET=/me
 # to get your credentials
 client = ovh.Client(
-    endpoint=config['endpoint'],
-    application_key=config['application_key'],
-    application_secret=config['application_secret'],
-    consumer_key=config['consumer_key'],
+    endpoint=config["endpoint"],
+    application_key=config["application_key"],
+    application_secret=config["application_secret"],
+    consumer_key=config["consumer_key"],
 )
 
 # Print nice welcome message
-print("Welcome", client.get('/me')['firstname'])
+print("Welcome", client.get("/me")["firstname"])
 
-headers = {'Accept': 'application/json','X-Ovh-Application':config['application_key'],'X-Ovh-Consumer':config['consumer_key'],
-'Content-Type':'application/json;charset=utf-8','Host':config['endpointAPI']}
+headers = {
+    "Accept": "application/json",
+    "X-Ovh-Application": config["application_key"],
+    "X-Ovh-Consumer": config["consumer_key"],
+    "Content-Type": "application/json;charset=utf-8",
+    "Host": config["endpointAPI"],
+}
 print("Preparing Package")
-#getting current time
-response = requests.get(f"https://{config['endpointAPI']}/1.0/auth/time", headers=headers)
+# getting current time
+response = requests.get(
+    f"https://{config['endpointAPI']}/1.0/auth/time", headers=headers
+)
 if response.status_code == 200:
     print("Getting Time")
 else:
@@ -29,48 +41,92 @@ else:
     print(json.dumps(response.json(), indent=4))
     exit()
 timeDelta = int(response.text) - int(time.time())
-#run for 8 days
+# run for 8 days
 for day in range(4):
-    print(f'Day {day}')
+    print(f"Day {day}")
     # creating a new cart
-    cart = client.post("/order/cart", ovhSubsidiary=config['ovhSubsidiary'], _need_auth=False)
-    #assign new cart to current user
+    cart = client.post(
+        "/order/cart", ovhSubsidiary=config["ovhSubsidiary"], _need_auth=False
+    )
+    # assign new cart to current user
     client.post("/order/cart/{0}/assign".format(cart.get("cartId")))
-    #put ks1 into cart
-    #result = client.post(f'/order/cart/{cart.get("cartId")}/eco',{"duration":"P1M","planCode":"22sk010","pricingMode":"default","quantity":1})
-    #apparently this shit sends malformed json whatever baguette
-    payload={'duration':'P1M','planCode':'22sk010','pricingMode':'default','quantity':1}
-    response = requests.post(f"https://{config['endpointAPI']}/1.0/order/cart/{cart.get('cartId')}/eco", headers=headers, data=json.dumps(payload))
+    # put ks1 into cart
+    # result = client.post(f'/order/cart/{cart.get("cartId")}/eco',{"duration":"P1M","planCode":"22sk010","pricingMode":"default","quantity":1})
+    # apparently this shit sends malformed json whatever baguette
+    payload = {
+        "duration": "P1M",
+        "planCode": "22sk010",
+        "pricingMode": "default",
+        "quantity": 1,
+    }
+    response = requests.post(
+        f"https://{config['endpointAPI']}/1.0/order/cart/{cart.get('cartId')}/eco",
+        headers=headers,
+        data=json.dumps(payload),
+    )
     if response.status_code != 200:
         print(response.status_code)
         print(json.dumps(response.json(), indent=4))
         exit()
-    #getting current cart
-    response = requests.get(f"https://{config['endpointAPI']}/1.0/order/cart/{cart.get('cartId')}")
+    # getting current cart
+    response = requests.get(
+        f"https://{config['endpointAPI']}/1.0/order/cart/{cart.get('cartId')}"
+    )
     if response.status_code != 200:
         print(response.status_code)
         print(json.dumps(response.json(), indent=4))
         exit()
-    #modify item for checkout
-    itemID = response.json()['items'][0]
+    # modify item for checkout
+    itemID = response.json()["items"][0]
     print(f'Getting current cart {cart.get("cartId")}')
-    #set configurations
-    configurations = [{'label':'region','value':config['region']},{'label':'dedicated_datacenter','value':config['dedicated_datacenter']},{'label':'dedicated_os','value':'none_64.en'}]
+    # set configurations
+    configurations = [
+        {"label": "region", "value": config["region"]},
+        {"label": "dedicated_datacenter", "value": config["dedicated_datacenter"]},
+        {"label": "dedicated_os", "value": "none_64.en"},
+    ]
     for entry in configurations:
-        response = requests.post(f"https://{config['endpointAPI']}/1.0/order/cart/{cart.get('cartId')}/item/{itemID}/configuration", headers=headers, data=json.dumps(entry))
+        response = requests.post(
+            f"https://{config['endpointAPI']}/1.0/order/cart/{cart.get('cartId')}/item/{itemID}/configuration",
+            headers=headers,
+            data=json.dumps(entry),
+        )
         if response.status_code == 200:
             print(f"Setting {entry}")
         else:
             print(response.status_code)
             print(json.dumps(response.json(), indent=4))
             exit()
-    #set options
-    options = [{'itemId':itemID,'duration':'P1M','planCode':'bandwidth-100-included-ks','pricingMode':'default','quantity':1},
-            {'itemId':itemID,'duration':'P1M','planCode':'noraid-1x1000sa-sk010','pricingMode':'default','quantity':1},
-            {'itemId':itemID,'duration':'P1M','planCode':'ram-4g-sk010','pricingMode':'default','quantity':1}
+    # set options
+    options = [
+        {
+            "itemId": itemID,
+            "duration": "P1M",
+            "planCode": "bandwidth-100-included-ks",
+            "pricingMode": "default",
+            "quantity": 1,
+        },
+        {
+            "itemId": itemID,
+            "duration": "P1M",
+            "planCode": "noraid-1x1000sa-sk010",
+            "pricingMode": "default",
+            "quantity": 1,
+        },
+        {
+            "itemId": itemID,
+            "duration": "P1M",
+            "planCode": "ram-4g-sk010",
+            "pricingMode": "default",
+            "quantity": 1,
+        },
     ]
     for option in options:
-        response = requests.post(f"https://{config['endpointAPI']}/1.0/order/cart/{cart.get('cartId')}/eco/options", headers=headers, data=json.dumps(option))
+        response = requests.post(
+            f"https://{config['endpointAPI']}/1.0/order/cart/{cart.get('cartId')}/eco/options",
+            headers=headers,
+            data=json.dumps(option),
+        )
         if response.status_code == 200:
             print(f"Setting {option}")
         else:
@@ -78,38 +134,56 @@ for day in range(4):
             print(json.dumps(response.json(), indent=4))
             exit()
     print("Package ready, waiting for stock")
-    #the order expires in about 3 days, we create a new one after 2 days
+    # the order expires in about 3 days, we create a new one after 2 days
     for check in range(17280):
         now = datetime.now()
         print(f'Run {check+1} {now.strftime("%H:%M:%S")}')
-        #wait for stock
-        response = requests.get('https://us.ovh.com/engine/apiv6/dedicated/server/datacenter/availabilities?excludeDatacenters=false&planCode=22sk010&server=22sk010')
+        # wait for stock
+        response = requests.get(
+            "https://us.ovh.com/engine/apiv6/dedicated/server/datacenter/availabilities?excludeDatacenters=false&planCode=22sk010&server=22sk010"
+        )
         if response.status_code == 200:
             stock = response.json()
             score = 0
-            for datacenter in stock[0]['datacenters']:
-                #if datacenter['availability'] != "unavailable": score = score +1
-                if datacenter['datacenter'] == "rbx":
+            for datacenter in stock[0]["datacenters"]:
+                # if datacenter['availability'] != "unavailable": score = score +1
+                if datacenter["datacenter"] == "rbx":
                     print(f'RBX {datacenter["availability"]}')
-                    if datacenter['availability'] != "unavailable": score = score +1
-                if datacenter['datacenter'] == "gra":
+                    if datacenter["availability"] != "unavailable":
+                        score = score + 1
+                if datacenter["datacenter"] == "gra":
                     print(f'GRA {datacenter["availability"]}')
-                    if datacenter['availability'] == "unavailable": score = score +1
+                    if datacenter["availability"] == "unavailable":
+                        score = score + 1
         else:
-            time.sleep(randint(5,10))
+            time.sleep(randint(5, 10))
             continue
-        #lets checkout boooyaaa
-        #if score >= 1:
+        # lets checkout boooyaaa
+        # if score >= 1:
         if score == 2:
-            #autopay should be set to true if you want automatic delivery, otherwise it will just generate a invoice
-            payload={'autoPayWithPreferredPaymentMethod':False,'waiveRetractationPeriod':False}
-            #prepare sig
+            # autopay should be set to true if you want automatic delivery, otherwise it will just generate a invoice
+            payload = {
+                "autoPayWithPreferredPaymentMethod": False,
+                "waiveRetractationPeriod": False,
+            }
+            # prepare sig
             target = f"https://{config['endpointAPI']}/1.0/order/cart/{cart.get('cartId')}/checkout"
             now = str(int(time.time()) + timeDelta)
             signature = hashlib.sha1()
-            signature.update("+".join([config['application_secret'], config['consumer_key'],'POST', target, json.dumps(payload), now]).encode('utf-8'))
-            headers['X-Ovh-Signature'] = "$1$" + signature.hexdigest()
-            headers['X-Ovh-Timestamp'] = now
+            signature.update(
+                "+".join(
+                    [
+                        config["application_secret"],
+                        config["consumer_key"],
+                        "POST",
+                        target,
+                        json.dumps(payload),
+                        now,
+                    ]
+                ).encode("utf-8")
+            )
+            headers["X-Ovh-Signature"] = "$1$" + signature.hexdigest()
+            headers["X-Ovh-Timestamp"] = now
             response = requests.post(target, headers=headers, data=json.dumps(payload))
             if response.status_code == 200:
                 print(response.status_code)
